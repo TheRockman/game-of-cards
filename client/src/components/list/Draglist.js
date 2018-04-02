6d13238f2bf716bcdc5c45a46a2a52536c6fcde6
TheRockman <TheRockman@users.noreply.github.com>
2018-04-02 16:17:29 +0200

    drag cards around

--- client/src/components/list/Draglist.js
+++ client/src/components/list/Draglist.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import Card from "../card/Card";
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 // fake data generator
 const getItems = count =>
-  Array.from({ length: count }, (v, k) => k).map(k => ({
-    id: `item-${k}`,
-    content: `item ${k}`,
-  }));
+Array.from({ length: count }, (v, k) => k).map(k => ({
+  id: `item-${k}`,
+  content: `item ${k}`,
+}));
 
 // a little function to help us with reordering the result
 const reorder = (list, startIndex, endIndex) => {
@@ -23,12 +24,10 @@ const grid = 8;
 const getItemStyle = (isDragging, draggableStyle) => ({
   // some basic styles to make the items look a bit nicer
   userSelect: 'none',
-  padding: grid * 2,
-  margin: `0 ${grid}px 0 0`,
 
   // change background colour if dragging
-  background: isDragging ? 'lightgreen' : 'grey',
-
+  background: isDragging ? 'lightgreen' : 'transparent',
+  height: '100%',
   // styles we need to apply on draggables
   ...draggableStyle,
 });
@@ -36,8 +35,8 @@ const getItemStyle = (isDragging, draggableStyle) => ({
 const getListStyle = isDraggingOver => ({
   background: isDraggingOver ? 'lightblue' : 'lightgrey',
   display: 'flex',
-  padding: grid,
-  overflow: 'auto',
+  overflowX: 'auto',
+  overflowY: 'hidden',
 });
 
 class Draglist extends Component {
@@ -70,39 +69,62 @@ class Draglist extends Component {
   // But in this example everything is just done in one place for simplicity
   render() {
     return (
-      <DragDropContext onDragEnd={this.onDragEnd}>
-        <Droppable droppableId="droppable" direction="horizontal">
+      <div>
+
+      <Droppable droppableId="droppable-1">
+      {(provided, snapshot) => (
+        <div
+        ref={provided.innerRef}
+        style={{ backgroundColor: snapshot.isDraggingOver ? 'lightblue' : 'lightgrey', minHeight: '250px' }}
+        {...provided.droppableProps}
+        >
+        {provided.placeholder}
+        </div>
+      )}
+      </Droppable>
+
+      <Droppable droppableId="droppable-2" direction="horizontal">
+      {(provided, snapshot) => (
+        <div
+        ref={provided.innerRef}
+        style={getListStyle(snapshot.isDraggingOver)}
+        {...provided.droppableProps}
+        >
+        {this.state.items.map((item, index) => (
+          <Draggable key={item.id} draggableId={item.id} index={index}>
           {(provided, snapshot) => (
+            <div>
             <div
-              ref={provided.innerRef}
-              style={getListStyle(snapshot.isDraggingOver)}
-              {...provided.droppableProps}
+            ref={provided.innerRef}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            style={getItemStyle(
+              snapshot.isDragging,
+              provided.draggableProps.style
+            )}
             >
-              {this.state.items.map((item, index) => (
-                <Draggable key={item.id} draggableId={item.id} index={index}>
-                  {(provided, snapshot) => (
-                    <div>
-                      <div
-                        ref={provided.innerRef}
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                        style={getItemStyle(
-                          snapshot.isDragging,
-                          provided.draggableProps.style
-                        )}
-                      >
-                        {item.name}
-                      </div>
-                      {provided.placeholder}
-                    </div>
-                  )}
-                </Draggable>
-              ))}
-              {provided.placeholder}
+
+            <Card
+            name="Flame of burnification"
+            description="Dont play with matches"
+            image="http://sf.co.ua/14/05/wallpaper-238879.jpg"
+            rules="Pyromania"
+            cost="1"
+            power="3"
+            toughness="0">
+            </Card>
+
+            </div>
+            {provided.placeholder}
             </div>
           )}
-        </Droppable>
-      </DragDropContext>
+          </Draggable>
+        ))}
+        {provided.placeholder}
+        </div>
+      )}
+      </Droppable>
+      </div>
     );
   }
 }
