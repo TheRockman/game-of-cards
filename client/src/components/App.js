557af8327a4ac54f56a30b56f1110b09571c5b5b
TheRockman <TheRockman@users.noreply.github.com>
2018-04-02 18:58:07 +0200

    header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './header/Header';
 
 import Playground from './Playground';
 
-const Header = () => <h2>Header</h2>;
+// const Header = () => <h2>Header</h2>;
 const Landing = () => <h2>Landing</h2>;
 const CreateDeck = () => <h2>CreateDeck</h2>;
+
+let user = {isLoggedIn: false, userName: 'Rando McRando'};
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <div>
+          <Header user={user}> </Header>
           <Route exact path="/" component={Landing} />
           <Route path="/playground" component={Playground} />
         </div>
