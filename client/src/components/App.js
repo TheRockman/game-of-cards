e038661be717e7b60fbf0b5842d19301808bde0c
Herman Dahlen <hman.dahlen@gmail.com>
2018-04-02 18:00:47 +0200

    router setup

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Header = () => <h2>Header</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
-  return <div>hej</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" component={Landing} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
